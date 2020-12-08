cae0a27c41c8c2480ae6c8bd21b707414d5aac53
Gorgoras <martinzurita1@gmail.com>
2020-12-08 11:56:37 -0300

    changes for second release

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ classifiers = [
 setup(
     name='pdAzTables',
     packages=find_packages(),
-    version='0.0.1',
-    download_url='https://github.com/Gorgoras/PandasAzureTables/archive/v0.0.1.tar.gz',
+    version='0.0.2',
+    download_url='https://github.com/Gorgoras/PandasAzureTables/archive/v0.0.2.tar.gz',
     description='Easy Azure table storage into Pandas dataframe',
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     long_description_content_type="text/markdown",
